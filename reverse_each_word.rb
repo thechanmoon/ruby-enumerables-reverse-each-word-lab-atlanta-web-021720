fa6d1f60fd085670f50ee891e033be6a0553a01f
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 18:06:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
-
-define reverse_each_word(sentence)
+def reverse_each_word(sentence)
   #sentence.split.collect{ |word| word.reverse}.join(" ")
   sentence.split.collect{ |word| word.reverse}.join(" ")
 end
-
-# def reverse_each_word(sentence)
-#   sentence.split.collect {|word| word.reverse}.join(" ")
-# end
